bfbf75f7948612b2f3a71205f813b0fc1d0707d9
Disuqi <disuqi.hijazi.7@gmail.com>
2021-09-23 21:13:37 +0100

    organized the code a bit and now the ladder works better

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -7,6 +7,7 @@ export class GameScene extends Phaser.Scene {
     hasKey;
     level;
     controlsOFF;
+    gameOver;
     //Text
     welcome;
     aboutMe;
@@ -30,117 +31,24 @@ export class GameScene extends Phaser.Scene {
             'I am capable and happy to work independently or unsupervised. Moreover, I am friendly and compassionate\n' +
             'which enables me to remain level-headed and interact with customers to ensure the best experience is provided.\n' +
             'I am currently a student at Salford University, studying computer science and looking for an internship or part-time job in the Technological industry.';
+        this.gameOver = false;
     }
 
     create() {
-        //background
-        this.add.image(0, 0, 'background').setOrigin(0, 0).setScale(4.5);
-
-        //arrowKeys
-        this.arrowKeys = this.physics.add.staticGroup();
-        this.up = this.arrowKeys.create(200, 200, 'w');
-        this.left = this.arrowKeys.create(140, 260, 'a');
-        this.down = this.arrowKeys.create(200, 260, 's');
-        this.right = this.arrowKeys.create(260, 260, 'd');
-        const listOfArrows = [this.up, this.down, this.left, this.right]
-        listOfArrows.forEach(arrow => arrow.setScale(0.6));
-
-        //key
-        this.key = this.physics.add.image(1170, 350, 'key');
-        this.key.setDepth(1);
-        this.key.setScale(0.05);
-        this.key.body.allowGravity = false;
-        this.tweens.add({
-            targets: this.key,
-            y: '+=10',
-            ease: 'Linear',
-            duration: 800,
-            yoyo: true,
-            repeat: -1,
-        })
-
-        //platforms
+        //Create Platforms background and items in the level
         this.platforms = this.physics.add.staticGroup();
         this.createLevel(this);
-
-        //coins
-        this.coins = this.physics.add.group({
-            key: 'coin',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 },
-        });
-        this.coins.children.iterate(function(child) {
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-            child.setScale(0.7);
-        });
-
-        //  score
-        this.scoreText = this.add.text(16, 16, 'Score:0', { fontSize: '32px', fill: '#ccffff', fontFamily: 'Arcade Interlaced' });
-
-        //player
-        this.player = this.physics.add.sprite(10, 620, 'finn').setScale(2);
+        //arrowKeys
+        this.createArrowKeys()
+            //score
+            // this.scoreText = this.add.text(16, 16, 'Score:0', { fontSize: '32px', fill: '#ccffff', fontFamily: 'Arcade Interlaced' });
+            //player
+        this.player = this.physics.add.sprite(600, 620, 'finn').setScale(2);
         this.player.setCollideWorldBounds(true);
-
-        //All animations
-        //  player animations
-        this.anims.create({
-            key: 'run',
-            frames: this.anims.generateFrameNumbers('finn', { start: 9, end: 14 }),
-            frameRate: 8,
-            repeat: -1,
-        }, );
-        this.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('finn', { start: 0, end: 8 }),
-            frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
-            key: 'jump',
-            frames: [{ key: 'finn', frame: 15 }],
-        })
-        this.anims.create({
-            key: 'die',
-            frames: this.anims.generateFrameNumbers('finn', { start: 19, end: 23 }),
-            frameRate: 8,
-        })
-        this.anims.create({
-            key: 'hit',
-            frames: this.anims.generateFrameNumbers('finn', { start: 17, end: 18 }),
-            frameRate: 6,
-        });
-        this.anims.create({
-            key: 'lookBack',
-            frames: [{ key: 'finn', frame: 28 }]
-        })
-        this.anims.create({
-            key: 'climb',
-            frames: this.anims.generateFrameNumbers('finn', { start: 29, end: 30 }),
-            frameRate: 8
-        })
-        this.anims.create({
-            key: 'computerOn',
-            frames: this.anims.generateFrameNumbers('computer', { start: 0, end: 10 }),
-            frameRate: 6,
-        });
-
-
+        //animations
+        this.createAnimations();
         //collissions and overlaps
-        this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.coins, this.platforms);
-        this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
-        this.physics.add.overlap(this.player, this.house, this.openDoor, null, this);
-        this.physics.add.overlap(this.player, this.key, this.getKey, null, this);
-        this.physics.add.overlap(this.player, this.ladder, this.climbLadder, null, this);
-        this.physics.add.overlap(this.player, this.computer, this.turnOnComputer, null, this);
-
-        //keyboard controls
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.cursors.a = this.input.keyboard.addKey('A', true, true);
-        this.cursors.s = this.input.keyboard.addKey('S', true, true);
-        this.cursors.d = this.input.keyboard.addKey('D', true, true);
-        this.cursors.w = this.input.keyboard.addKey('W', true, true);
-        this.gameOver = false;
+        this.createColliders()
     }
 
     update() {
@@ -156,6 +64,12 @@ export class GameScene extends Phaser.Scene {
         if (!this.climbing) {
             this.player.body.allowGravity = true;
         }
+        //movement left right and jump
+        this.movePlayer();
+        //arrowKeys image
+        this.updateKeysImage();
+    }
+    movePlayer() {
         //left, right and idle
         if (this.cursors.left.isDown || this.cursors.a.isDown) {
             this.run(-1);
@@ -165,10 +79,10 @@ export class GameScene extends Phaser.Scene {
             this.run(1);
             this.player.anims.play('run', true);
             this.player.resetFlip();
-        } else if (this.cursors.down.isDown || this.cursors.s.isDown) {
+        } else if ((this.cursors.down.isDown || this.cursors.s.isDown) && !this.climbing) {
             this.player.anims.play('lookBack', true);
             this.player.setVelocityX(0);
-        } else {
+        } else if (!this.climbing) {
             this.player.setVelocityX(0);
             this.player.anims.play('idle', true);
         }
@@ -181,13 +95,8 @@ export class GameScene extends Phaser.Scene {
         if (this.player.body.velocity.y != 0 && !this.climbing) {
             this.player.anims.play('jump');
         }
-        if (this.climbing) {
-            this.player.anims.play('climb', true);
-        }
-        //arrowKeys image
-        this.changeKeysImage();
     }
-    changeKeysImage() {
+    updateKeysImage() {
         if (this.cursors.up.isDown || this.cursors.w.isDown) {
             this.up.setTexture('wP');
             this.up.x = 203;
@@ -229,36 +138,10 @@ export class GameScene extends Phaser.Scene {
         this.player.setVelocityX(200 * direction);
     }
 
-    collectCoin(player, coin) {
-        coin.disableBody(true, true);
-
-        this.score += 10;
-        this.scoreText.setText('Score:' + this.score);
-
-        if (this.coins.countActive(true) == 0) {
-            this.coins.children.iterate(function(child) {
-                child.enableBody(true, child.x, 0, true, true);
-            });
-        }
-    }
-
     openDoor(player, door) {
         if (this.hasKey == true && this.player.body.blocked.down) {
             this.house.setFrame(1)
             this.controlsOFF = true;
-            if (this.player.x > 925) {
-                this.player.resetFlip();
-                this.player.toggleFlipX();
-            }
-            this.player.anims.play('run', true);
-            this.tweens.add({
-                targets: this.player,
-                x: 925,
-                duration: Math.abs(this.player.x - 925 + 1000),
-                ease: 'Linear',
-            }).on('complete', () => {
-                this.player.anims.play('idle', true);
-            });
             this.level += 1;
             this.createLevel(this);
         }
@@ -273,9 +156,14 @@ export class GameScene extends Phaser.Scene {
     climbLadder(player, ladder) {
         if (this.cursors.up.isDown) {
             this.player.anims.play('climb', true);
-            this.player.y -= 3;
-            this.player.body.allowGravity = false;
+            this.player.setVelocityY(-200);
             this.climbing = true;
+        } else if (this.cursors.down.isDown && this.climbing) {
+            this.player.anims.play('climb', true);
+            this.player.setVelocityY(200);
+        } else if (this.climbing) {
+            this.player.anims.stop();
+            this.player.setVelocityY(0);
         }
 
     }
@@ -317,22 +205,39 @@ export class GameScene extends Phaser.Scene {
         this.platforms.clear(true, true);
         switch (this.level) {
             case 0:
-                //House & door
+                //Background
+                this.add.image(0, 0, 'background').setOrigin(0, 0).setScale(4.5);
+                //House
                 this.house = this.physics.add.image(1050, 520, 'house');
                 this.house.body.allowGravity = false;
                 this.house.body.setSize(50, 80, true);
+                this.house.body.setOffset(0, 200);
+                //Ladder
                 this.ladder = this.physics.add.image(1060, 570, 'ladder');
                 this.ladder.body.allowGravity = false;
                 this.ladder.setScale(0.5);
+                //Computer
                 this.computer = this.physics.add.sprite(250, 550, 'computer');
                 this.computer.setScale(0.5);
                 this.computer.toggleFlipX();
                 this.computer.body.allowGravity = false;
                 this.computerOn = false;
-                //const welcomeText = game.add.text(0, 50, this.welcome, { fontFamily: 'Munro', fontSize: '40px', color: '#ccffff', backgroundColor: '#5E6664', fixedWidth: 3000, padding: 20 })
                 this.physics.add.image()
                 this.platforms.create(480, 680, 'ground');
                 this.platforms.create(1446, 680, 'ground');
+                //key
+                this.key = this.physics.add.image(1170, 350, 'key');
+                this.key.setDepth(1);
+                this.key.setScale(0.05);
+                this.key.body.allowGravity = false;
+                this.tweens.add({
+                    targets: this.key,
+                    y: '+=10',
+                    ease: 'Linear',
+                    duration: 800,
+                    yoyo: true,
+                    repeat: -1,
+                });
                 break;
             case 1:
                 this.platforms.create(480, 680, 'ground');
@@ -350,5 +255,70 @@ export class GameScene extends Phaser.Scene {
                 this.platforms.create(500, 200, 'platform');
                 break;
         }
+    }
+
+    createArrowKeys() {
+        this.arrowKeys = this.physics.add.staticGroup();
+        this.up = this.arrowKeys.create(200, 200, 'w');
+        this.left = this.arrowKeys.create(140, 260, 'a');
+        this.down = this.arrowKeys.create(200, 260, 's');
+        this.right = this.arrowKeys.create(260, 260, 'd');
+        const listOfArrows = [this.up, this.down, this.left, this.right]
+        listOfArrows.forEach(arrow => arrow.setScale(0.6));
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.cursors.a = this.input.keyboard.addKey('A', true, true);
+        this.cursors.s = this.input.keyboard.addKey('S', true, true);
+        this.cursors.d = this.input.keyboard.addKey('D', true, true);
+        this.cursors.w = this.input.keyboard.addKey('W', true, true);
+    }
+
+    createAnimations() {
+        this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('finn', { start: 9, end: 14 }),
+            frameRate: 8,
+            repeat: -1,
+        }, );
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('finn', { start: 0, end: 8 }),
+            frameRate: 5,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: 'jump',
+            frames: [{ key: 'finn', frame: 15 }],
+        })
+        this.anims.create({
+            key: 'die',
+            frames: this.anims.generateFrameNumbers('finn', { start: 19, end: 23 }),
+            frameRate: 8,
+        })
+        this.anims.create({
+            key: 'hit',
+            frames: this.anims.generateFrameNumbers('finn', { start: 17, end: 18 }),
+            frameRate: 6,
+        });
+        this.anims.create({
+            key: 'lookBack',
+            frames: [{ key: 'finn', frame: 28 }]
+        })
+        this.anims.create({
+            key: 'climb',
+            frames: this.anims.generateFrameNumbers('finn', { start: 29, end: 30 }),
+            frameRate: 4
+        })
+        this.anims.create({
+            key: 'computerOn',
+            frames: this.anims.generateFrameNumbers('computer', { start: 0, end: 10 }),
+            frameRate: 5,
+        });
+    }
+    createColliders() {
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.overlap(this.player, this.house, this.openDoor, null, this);
+        this.physics.add.overlap(this.player, this.key, this.getKey, null, this);
+        this.physics.add.overlap(this.player, this.ladder, this.climbLadder, null, this);
+        this.physics.add.overlap(this.player, this.computer, this.turnOnComputer, null, this);
     }
 }
